668692990ee155c4ac9d0096d0713cbbefff2fa9
Erick Gilmar Martinez Capcha <erickmartinezc@gmail.com>
2026-07-11 13:16:27 -0500

    chore: actualizar archivos de la tienda web

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,33 +1,3 @@
-export interface ProductImage {
-  url: string;
-  alt: string;
-}
-
-export interface Product {
-  id: string;
-  title: string;
-  slug: string;
-  description: string;
-  price: number;
-  currency: string;
-  images: ProductImage[];
-  categoryId: string;
-  tags: string[];
-  metadata_vector: number[];
-  view_count: number;
-  stock: number;
-  isFeatured: boolean;
-  createdAt: Date | string;
-  updatedAt: Date | string;
-}
-
-export interface Category {
-  id: string;
-  name: string;
-  slug: string;
-  order: number;
-}
-
 export interface NewsOrOffer {
   id: string;
   type: 'banner' | 'news' | 'event';
